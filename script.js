b95068d955ff091090148ae34f67090ee64ad679
Neha Desai <nehadesai247@gmail.com>
2023-10-17 15:21:02 -0700

    Modify what the buttons do and how they do it
    Deleting the grid squares was previously handled by the clear function,
    which responded to a click on the clear button. However, this required
    the user to always clear the grid prior to setting a new size.
    
    I changed the functionality of the size button to clear the grid
    (delete all the grid squares) before creating a new grid - whenever a
    new size is entered.
    
    The clear function, now, just whites out a grid (sets the
    color of all grid squares to white).
    
    I also refactored the code for the two color options so that event
    listeners aren't unnecessarily created each time the color is changed.

--- script.js
+++ script.js
@@ -5,42 +5,80 @@ let black = document.querySelector("#black");
 let multicolor = document.querySelector("#multicolor");
 let grid_squares = document.getElementsByClassName("squares");
 let clear = document.querySelector("#clear");
-
+let is_multicolor = false;
+let size_clicked_once = false;
+let GRID_SIZE = 500;
 
 size_button.addEventListener('click', () => {
-    size_input = Number(prompt("Please enter a size:"));
+    if (!size_clicked_once) {
+        size_input = Number(prompt("Please enter a size:"));
 
-    let total_squares = size_input * size_input;
+        let total_squares = size_input * size_input;
 
-    for (let i = 1; i <= total_squares; i++) {
-        let one_square = document.createElement("div");
+        for (let i = 1; i <= total_squares; i++) {
+            let one_square = document.createElement("div");
 
-        one_square.className = "squares";
+            one_square.className = "squares";
         
-        one_square.style.width = 600/(size_input) + "px";
-        one_square.style.height = 600/(size_input) + "px";
+            one_square.style.width = GRID_SIZE/(size_input) + "px";
+            one_square.style.height = GRID_SIZE/(size_input) + "px";
         
-        grid_container.appendChild(one_square);
+            one_square.addEventListener('mouseover', () => {
+                one_square.style.backgroundColor = getColor();
+            });
+
+            grid_container.appendChild(one_square);
+        }
+        size_clicked_once = true;
+    }
+
+    else {
+        let nodes_to_clear = Array.from(grid_squares);
+        
+        for (let i = 0; i < nodes_to_clear.length; i++) {
+            nodes_to_clear[i].remove();
+        }
+
+        size_input = Number(prompt("Please enter a size:"));
+
+        let total_squares = size_input * size_input;
+
+        for (let i = 1; i <= total_squares; i++) {
+            let one_square = document.createElement("div");
+
+            one_square.className = "squares";
+        
+            one_square.style.width = GRID_SIZE/(size_input) + "px";
+            one_square.style.height = GRID_SIZE/(size_input) + "px";
+        
+            one_square.addEventListener('mouseover', () => {
+                one_square.style.backgroundColor = getColor();
+            });
+
+            grid_container.appendChild(one_square);
+        }
     }
 });
 
 
 black.addEventListener('click', () => {
-    for (let i = 0; i < grid_squares.length; i++) {
-        grid_squares[i].addEventListener('mouseover', () => {
-            grid_squares[i].style.backgroundColor = "black";
-        });
-    }
+    is_multicolor = false;
 });
 
 
 multicolor.addEventListener('click', () => {
-    for (let i = 0; i < grid_squares.length; i++) {
-        grid_squares[i].addEventListener('mouseover', () => {
-            grid_squares[i].style.backgroundColor = "#" + randomColor();
-        });
-    }
+    is_multicolor = true;
 });
+
+
+function getColor() {
+    if (is_multicolor) {
+        return "#" + randomColor();
+    }
+    else {
+        return "black";
+    }
+}
 
 
 function randomColor() {
@@ -49,9 +87,9 @@ function randomColor() {
 
 
 clear.addEventListener('click', () => {
-    let nodes_to_delete = Array.from(grid_squares);
+    let nodes_to_white = Array.from(grid_squares);
 
-    for (let i = 0; i < nodes_to_delete.length; i++) {
-        nodes_to_delete[i].remove();
+    for (let i = 0; i < nodes_to_white.length; i++) {
+        nodes_to_white[i].style.backgroundColor = "white";
     }
 });
